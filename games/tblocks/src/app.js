22e9c436a08d54b8e4dbe9b89dce8e1311788560
lukesUbuntu <luke@hardiman.co.nz>
2015-09-11 00:03:29 +1200

    Moved image dropping to basic object so can render mutliple image drops. @todo bug with position.top() not been referened

--- games/tblocks/src/app.js
+++ games/tblocks/src/app.js
@@ -20,8 +20,79 @@ var gameModule = {
     },
     scaleWidth : function(pert,element){//render a % of element with
         return (typeof pert != "undefined") ? $(element).width() * pert / 100 :  $(element).width();
+    },
+    isElement : function($element){ //checks if we have a valid jquery Object & element exists
+        return (typeof $element != "undefined" && $element instanceof jQuery && $element.length);
+    }
+
+};
+
+/**
+ * Drops an div/image to bottom of game_grid but clones our image holder
+ */
+var dropImage = {
+    maxImages: 6,       //max images to drop down
+    currentImage : 0,   //current image index
+    position : null,
+    count : function(){
+        return
+    },
+    drop: function($image_block,$game_grid){//can define image_block or game_grid
+        //if we don't pass anything lets just grab the standard element locations
+        $image_block = (gameModule.isElement($image_block)) ? $image_block : $("#image_block");
+        $game_grid = (gameModule.isElement($game_grid)) ? $game_grid : $("#game_grid");
+
+        //clone our image block div
+        var image_block = $image_block.clone();
+        console.log("app.js dropImage running");
+
+        //if we don't have a current position
+        if (dropImage.position == null);
+        dropImage.position = image_block.position();
+
+        //setup our grid blocks append our hidden div elements
+        $game_grid.append(image_block);
+        $game_grid.append(word_blocks);
+
+        //lets just display where our image is for testing
+        image_block.show();
+        image_block.css('background-color' , 'black');//jst so i can see where its rendering lol
+
+        //drop image test
+        var windowHeight = $(window).height();
+        var lineHeight = $game_grid.height();
+
+        console.log("this.position()",dropImage.position)
+
+        var desiredBottom = dropImage.position.top + 20;     //desired position offset 20
+        var speed   = 2 * 1000; //2 second move
+
+        var newPosition = windowHeight - (lineHeight - desiredBottom);
+        //newPosition
+        console.log("newPosition",newPosition);
+        console.log("desiredBottom",desiredBottom);
+        console.log("image_block.position()",image_block.position());
+        //desiredBottom
+
+        image_block.animate({top:$game_grid.height()},speed,function () {
+            image_block.css({
+                bottom: desiredBottom,
+                top: 'auto'
+            });
+            dropImage.position = image_block.position();
+            console.log("image_block.position()",image_block.position())
+        });
+
     }
 };
+
+
+
+
+
+
+
+
 
 /**
  * On Splash Screen Load
@@ -91,33 +162,6 @@ $(document).on('pageinit','#start_game',function(){
 
     console.log("app.js finished game setup");
 
-    //setup our grid blocks append our hidden div elements
-    game_grid.append(image_block);
-    game_grid.append(word_blocks);
-
-    //lets just display where our image is for testing
-    image_block.show();
-    image_block.css('background-color' , 'black');//jst so i can see where its rendering lol
-
-    //drop image test
-    var windowHeight = $(window).height();
-    var lineHeight = game_grid.height();
-    var desiredBottom = 20;     //desired ending
-    var speed   = 2 * 1000; //2 second move
-
-    var newPosition = windowHeight - (lineHeight - desiredBottom);
-    //newPosition
-    console.log("newPosition",newPosition);
-    console.log("desiredBottom",desiredBottom)
-
-    //desiredBottom
-    image_block.animate({top:game_grid.height()},speed,function () {
-        image_block.css({
-            bottom: desiredBottom,
-            top: 'auto'
-        });
-    });
-
     //startup end
     /*
      element.style {
@@ -128,8 +172,52 @@ $(document).on('pageinit','#start_game',function(){
      width: 200px;
      }
      */
-
+    //dropImage(image_block,game_grid);
+    dropImage.drop();
+//    dropImage.drop();
 });
+/**
+ * Drops an div/image to bottom of game_grid but clones our image holder
+ */
+function dropImage($image_block,$game_grid){
+    //if we don't pass anything lets just grab the standard element locations
+    $image_block = (gameModule.isElement($image_block)) ? $image_block : $("#image_block");
+    $game_grid = (gameModule.isElement($game_grid)) ? $game_grid : $("#game_grid");
+
+    //clone our image block div
+    var image_block = $image_block.clone();
+    console.log("app.js dropImage running")
+    //setup our grid blocks append our hidden div elements
+    $game_grid.append(image_block);
+    $game_grid.append(word_blocks);
+
+    //lets just display where our image is for testing
+    image_block.show();
+    image_block.css('background-color' , 'black');//jst so i can see where its rendering lol
+
+    //drop image test
+    var windowHeight = $(window).height();
+    var lineHeight = $game_grid.height();
+    var desiredBottom = 20;     //desired ending
+    var speed   = 2 * 1000; //2 second move
+
+    var newPosition = windowHeight - (lineHeight - desiredBottom);
+    //newPosition
+    console.log("newPosition",newPosition);
+    console.log("desiredBottom",desiredBottom);
+    console.log("image_block.position()",image_block.position());
+    //desiredBottom
+
+    image_block.animate({top:$game_grid.height()},speed,function () {
+        image_block.css({
+            bottom: desiredBottom,
+            top: 'auto'
+        });
+
+        console.log("image_block.position()",image_block.position())
+    });
+
+}
 /**
  * Fake Loading Demo lol
  */
