f5817190bfd0e3e3ddfd0064ca54d9599538a8ad
Luke <=luke@hardiman.co.nz>
2015-09-23 15:55:44 +1200

    Updating server from school -added get/add feather to api

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -19,12 +19,14 @@ class IndexController extends ControllerBase
     public function indexAction()
     {
 
-
-
         $this->loginCheck();
         //load our games into view
-        $games = \Game::find();
+
+        $games = \Game::find();//get our games
+        $user = Users::findFirst(Sentry::getUser()->id); //get user
+
         $this->view->setVar("Games",$games);
+        $this->view->setVar("User", $user);
     }
 
 
