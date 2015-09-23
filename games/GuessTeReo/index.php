d5f525a0d7821462e11202b08ed9713d9357732c
Luke <=luke@hardiman.co.nz>
2015-09-24 10:10:48 +1200

    brining master upto date

--- games/GuessTeReo/index.php
+++ games/GuessTeReo/index.php
@@ -4,66 +4,31 @@ set_include_path('.\includes');
 
 //Includes
 //require_once 'connect.php';
-require 'head.php';
+require_once 'head.php';
+require_once 'header.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <body>
-<div data-role="header">
-    <a href="../../index.php" class="button">Home</a>
-    <a href="includes/scoreboard.php" class="button">Scoreboard</a>
-</div>
-
 <div data-role="main" class="ui-content">
     <div class="img">
-        <a><img src="testImages/img4.jpg" alt="Game Image One" style="width:180px;
+        <a><img src="src/gameImages/img4.jpg" alt="Game Image One" style="width:180px;
                     height:120px;"></a>
-        <a><img src="testImages/img2.jpg" alt="Game Image Two" style="width:180px;
+        <a><img src="src/gameImages/img2.jpg" alt="Game Image Two" style="width:180px;
                     height:120px;"></a>
         </div>
     <div class="img">
-        <a><img src="testImages/img3.jpg" alt="Game Image Three" style="width:180px;
+        <a><img src="src/gameImages/img3.jpg" alt="Game Image Three" style="width:180px;
                     height:120px;"></a>
-        <a><img src="testImages/img4.jpg" alt="Game Image Four" style="width:180px;
+        <a><img src="src/gameImages/img4.jpg" alt="Game Image Four" style="width:180px;
                     height:120px;"></a>
     </div>
 
-    <div id="guessWord">
-        <input id="write" type="text">
+    <?php
+    require_once 'guessSection.php';
+    require_once 'keyboard.php';
+    ?>
     </div>
-
-    <div id="keyboardContainer">
-        <button id="gameInput" type="button">p</button>
-        <button id="gameInput" type="button">h</button>
-        <button id="gameInput" type="button">k</button>
-        <button id="gameInput" type="button">m</button>
-        <button id="gameInput" type="button">n</button>
-
-        <button id="gameInput" type="button">a</button>
-        <button id="gameInput" type="button">e</button>
-        <button id="gameInput" type="button">i</button>
-        <button id="gameInput" type="button">o</button>
-        <button id="gameInput" type="button">u</button>
-
-        <button id="gameInput" type="button">ā</button>
-        <button id="gameInput" type="button">ē</button>
-        <button id="gameInput" type="button">ī</button>
-        <button id="gameInput" type="button">ō</button>
-        <button id="gameInput" type="button">ū</button>
-
-        <button id="gameInput" type="button">Ng</button>
-        <button id="gameInput" type="button">r</button>
-        <button id="gameInput" type="button">t</button>
-        <button id="gameInput" type="button">w</button>
-        <button id="gameInput" type="button">Wh</button>
-
-        <div id="specialButtons">
-            <button class="delete" type="button">Delete</button>
-            <button class="space" type="button">Space</button>
-            <button class="return" type="button">✅</button>
-        </div>
-    </div>
-</div>
 </body>
 </html>
