626a3d79e055c1665ced60cd90ac675a65ddcfc5
lukesUbuntu <luke@hardiman.co.nz>
2015-09-24 18:38:11 +1200

    Full push

---
+++ games/gameHangiman/game.php
@@ -0,0 +1,194 @@
+<?php
+include('getUserInfo.php');
+include('checkGameStatus.php')
+?>
+<html>
+	<head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link href = "css/main.css" rel = "stylesheet" type = "text/css">
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+        <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+		<title>Hangiman</title>
+	</head>
+	<body>
+	<!--
+	<div data-role="page" id="menu">
+		<h1>Hangiman</h1>
+		<a href="#game" data-role="button">Play Game</a>
+		<a href="#instructions" data-role="button">Instructions</a>
+		<a href="#highscores" data-role="button">High Scores</a>
+	</div>
+-->
+    <div data-role="page" id="game">
+        <div data-role="main">
+            <div id="infobar" class="ui-bar ui-bar-a">
+
+                <h4 id="FoodRemaining" style="float: left;">Lives Remaining: X</h4>
+                <h4 id="Score" style="float: right;">Score: XXXX</h4>
+
+            </div>
+
+
+            <div id="Hangi">
+                <img id="HangiImage" src="images/hangiPlaceholder.gif">
+            </div>
+
+            <div id="WordToGuess">
+                *Word Goes here*
+            </div>
+            <div id="GameOptions">
+                <button id="ButtonHint" data-role="button" data-inline="true" class="btn" data-mini="true">Use Hint (5x Feathers)</button>
+                <button id="ButtonForfeit" data-role="button" data-inline="true" class="btn" data-mini="true">Forfeit Game</button>
+            </div>
+            <div id="Keyboard">
+                <div class="KeyboardRow">
+                    <span class="KeyboardKey">q</span>
+                    <span class="KeyboardKey">w</span>
+                    <span class="KeyboardKey">e</span>
+                    <span class="KeyboardKey">r</span>
+                    <span class="KeyboardKey">t</span>
+                    <span class="KeyboardKey">y</span>
+                    <span class="KeyboardKey">u</span>
+                    <span class="KeyboardKey">i</span>
+                    <span class="KeyboardKey">o</span>
+                    <span class="KeyboardKey">p</span>
+                </div>
+                <div class="KeyboardRow">
+                    <span class="KeyboardKey">a</span>
+                    <span class="KeyboardKey">s</span>
+                    <span class="KeyboardKey">d</span>
+                    <span class="KeyboardKey">f</span>
+                    <span class="KeyboardKey">g</span>
+                    <span class="KeyboardKey">h</span>
+                    <span class="KeyboardKey">j</span>
+                    <span class="KeyboardKey">k</span>
+                    <span class="KeyboardKey">l</span>
+                </div>
+                <div class="KeyboardRow">
+                    <span class="KeyboardKey">z</span>
+                    <span class="KeyboardKey">x</span>
+                    <span class="KeyboardKey">c</span>
+                    <span class="KeyboardKey">v</span>
+                    <span class="KeyboardKey">b</span>
+                    <span class="KeyboardKey">n</span>
+                    <span class="KeyboardKey">m</span>
+                </div>
+            </div>
+        </div>
+
+        <div data-role="page" id="LostGame">
+            <h1>lost</h1>
+        </div>
+        <div data-role="page" id="WonGame">
+            <h1>won</h1>
+        </div>
+	</body>
+
+<script>
+    <?php
+//$gameScore = 3;
+ ?>
+    var id = <?php echo $id; ?>;
+    var gameScore = <?php echo $gameScore; ?>;
+    var livesRemaining = <?php echo $livesRemaining; ?>;
+    var incorrectGuesses = <?php echo $incorrectGuesses; ?>;
+    var lettersGuessed = '<?php echo "$lettersGuessed"; ?>';
+    var wordToDisplay = '<?php echo $wordToDisplay; ?>';
+    var gameProgress = '<?php echo "$gameProgress"; ?>';
+
+    $( document).ready(function() {
+        updateGameScreen();
+    });
+
+    $(".KeyboardKey").click(function(){
+        var $keyLetter = $(this).text();
+        //
+        $.ajax({
+            type:"POST",
+            cache:false,
+            url:"processInput.php",
+            data: //Must include user login
+            {
+                "id" : id,
+                "letter" : $keyLetter
+            },
+            dataType: "jsonp",
+            success: function(response){
+                if (response.success == true){
+                    gameScore = response.gameScore;
+                    livesRemaining = response.livesRemaining;
+                    incorrectGuesses = response.incorrectGuesses;
+                    lettersGuessed = response.lettersGuessed;
+                    wordToDisplay = response.wordToDisplay;
+                    gameProgress = response.gameProgress;
+                    updateGameScreen();
+                }
+                console.log(response);
+            },
+            failure: function(){
+                alert("Failed to contact server, please try again");
+            }
+        });
+    });
+
+    $("#ButtonHint").click(function(){
+        $.ajax({
+            type:"POST",
+            cache:false,
+            url:"processHint.php",
+            data: //Must include user login
+            {
+                "id" : id
+            },
+            dataType: "jsonp",
+            success: function(response){
+                if (response.success == true){
+                    gameScore = response.gameScore;
+                    livesRemaining = response.livesRemaining;
+                    lettersGuessed = response.lettersGuessed;
+                    wordToDisplay = response.wordToDisplay;
+                    currentPage = response.currentPage;
+                    gameProgress = response.gameProgress;
+                    updateGameScreen();
+                }
+                console.log(response);
+            },
+            failure: function(){
+                alert("Failed to contact server, please try again");
+            }
+        });
+    });
+
+    $("#ButtonForfeit").click(function(){
+        if(confirm("Are you sure you want to forfeit the game?")){
+			window.location.href='lostGame.php';
+        }
+    });
+
+    function updateGameScreen(){
+        if (lettersGuessed.length > 0){
+            for (var i=0; i < lettersGuessed.length; i++) {
+                letterToMark = lettersGuessed.charAt(i);
+                $("span:contains('" + letterToMark + "')").css("background-color", "black");
+                console.log(letterToMark);
+            }
+        }
+        $("#Score").text("Score: " + gameScore);
+        $("#FoodRemaining").text("Food Baskets: " + incorrectGuesses);
+        $("#WordToGuess").text(wordToDisplay);
+        if( gameProgress == 'won'){
+            window.location.href='wonGame.php';
+        }
+        else if( gameProgress == 'lost'){
+            window.location.href='lostRound.php';
+        }
+        else if(gameProgress == 'gameover'){
+            window.location.href='lostGame.php';
+        }
+    }
+
+</script>
+
+</html>
---
+++ games/gameHangiman/processInput.php
@@ -0,0 +1,113 @@
+<?php
+include 'noMacron.php';
+$objMacron = new noMacron();
+
+header ("Content-Type:text/xml");
+$con = mysqli_connect("127.0.0.1","toa","toa123","gameHangiman");
+if (mysqli_connect_errno())
+{
+ //echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+//check Login, get ID
+$id = $_POST["id"];
+
+$con -> set_charset("utf8");
+$sql = "SELECT * FROM in_progress WHERE userID = $id";
+
+if ($result=mysqli_query($con,$sql)){
+ $row = mysqli_fetch_row($result);
+    $gameScore = $row[1];
+    $livesRemaining = $row[2];
+    $totalFeathersEarned = $row[3];
+    $roundNumber = $row[4];
+    $incorrectGuesses = $row[5];
+    $lettersGuessedArray = preg_split('//u',$row[6], -1, PREG_SPLIT_NO_EMPTY); //ensures Maori characters are properly encoded within string
+    $wordBeingGuessed = preg_split('//u',$row[7], -1, PREG_SPLIT_NO_EMPTY); //ensures Maori characters are properly encoded within string
+    $gameProgress = $row[8];
+}
+
+$inputLetter = $_POST["letter"];
+
+
+
+if($gameProgress == 'running'){
+
+    $currentPage = '#game';
+    $success =  true;
+    //Check input against wordBeingGuessedArray, determines if letter is in word. If it isn't, +1 to incorrectGuesses. Add to lettersGuessedArray. Create string to be displayed on client
+
+    //determines if guessed letter is in word
+    //Removes macron
+    $gameWin = true;
+    if(!in_array($inputLetter, $lettersGuessedArray)){
+        array_push($lettersGuessedArray, $inputLetter);
+        if(!in_array($inputLetter, $wordBeingGuessed)){
+
+        }
+        $letterIsInWord = false;
+        foreach ($wordBeingGuessed as $letterInWord) {
+            if (strcasecmp($objMacron->removeMacron($letterInWord), $inputLetter) == 0) {
+                $letterIsInWord = true;
+				$gameScore += 10;
+            }
+        }
+        if(!$letterIsInWord){
+            $incorrectGuesses--;
+			$gameScore -= 10;
+        }
+    }
+    //Writes word plus missing letters to display on client screen
+    $wordToDisplay = "";
+    foreach($wordBeingGuessed as $letterInWord){
+        $letterExists = false;
+        foreach ($lettersGuessedArray as $letterGuessed) {
+            if (strcasecmp($objMacron->removeMacron($letterInWord), $letterGuessed) == 0) {
+                $wordToDisplay .= "{$letterInWord} ";
+                $letterExists = true;
+            }
+        }
+        if(!$letterExists){
+            $wordToDisplay .= "_ ";
+            $gameWin = false;
+        }
+    }
+
+    if($gameWin == true){
+        $gameProgress = 'won';
+        $totalFeathersEarned++;
+    }
+    else if($incorrectGuesses <= 0){
+        $gameProgress = 'lost';
+        $livesRemaining--;
+        if($livesRemaining <= 0){
+            $gameProgress = 'gameover';
+        }
+    }
+
+    $lettersGuessedString = join($lettersGuessedArray);
+    $sql = "UPDATE in_progress SET `gameScore` = $gameScore, `livesRemaining` = $livesRemaining, `totalFeathersEarned` = $totalFeathersEarned, `roundNumber` = $roundNumber, `incorrectGuesses` = $incorrectGuesses, `lettersGuessed` = '$lettersGuessedString', `gameProgress` = '$gameProgress', `wordToDisplay` = '$wordToDisplay' WHERE `userID` = $id";
+    mysqli_query($con,$sql);
+
+
+}else{
+    $success = false;
+}
+
+mysqli_close($con);
+
+$json = json_encode(array(
+    'success' => $success,
+    'gameScore' => $gameScore,
+    'livesRemaining' => $livesRemaining,
+    'incorrectGuesses' => $incorrectGuesses,
+    'lettersGuessed' => $lettersGuessedString,
+    'wordToDisplay' => $wordToDisplay,
+    'gameProgress' => $gameProgress));
+
+//set response to send back check for callback
+echo isset($_GET['callback'])
+    ? "{$_GET['callback']}($json)"
+    : $json;
+
+
+ ?>
