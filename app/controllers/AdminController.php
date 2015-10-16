63ab0a2592d70ea3b00ef941de7a7ab6d1b9f7bf
Luke Hardiman <luke@hardiman.co.nz>
2015-10-16 16:51:25 +1300

    Admin Section -added admin check -added admin section -added url redirect on login

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,10 +1,31 @@
 <?php
 
-class AdminController extends \Phalcon\Mvc\Controller
+class AdminController extends ControllerBase
 {
 
     public function indexAction()
     {
+
+        //check user is logged in and admin
+        $this->loginCheck('admin');
+
+        //get the current user logged in
+        $userProfile = Users::findFirst(Sentry::getUser()->id); //get user
+
+        $user = Sentry::findUserByID(1);
+
+        //get admin group and check if user is an admin
+        $admin = Sentry::findGroupByName('Administrator');
+
+        //scores
+        // Check if the user is in the administrator group
+        if (!$user->inGroup($admin)) {
+            // User is not in Administrator group
+            die ("Not Administrator Account");//@todo return user to an error page
+        }
+
+        $this->view->setVar("User", $userProfile);
+
 
     }
 
