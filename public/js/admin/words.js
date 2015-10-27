258b1c1a611c3a65e2e2e6af41252d59b1f52ef5
Luke Hardiman <luke@hardiman.co.nz>
2015-10-27 17:14:59 +1300

    Updating Admin/Words edit -Updating public folder/merging dev/toa

--- public/js/admin/words.js
+++ public/js/admin/words.js
@@ -2,5 +2,21 @@
  * Created by 9901623 on 21/10/2015.
  */
 $(document).ready(function(){
+    //bb
     $('#wordLists').DataTable();
+
+    //words modal
+    var editWord = $("#editWord");
+
+    //bind our buttons
+    $(editWord).on('show.bs.modal', function(e) {
+        var editDetails = $(e.relatedTarget).data('record');
+        console.log('editDetails',editDetails)
+        $(this).find('.danger').click(function(e){
+            console.log('closing');
+        });
+    });
+
 });
+
+//bind our edit functions to edit modal
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,5 +5,5 @@ console.log("loaded scripts.js");
 
 $(document).ready(function(){
     //if we have a global custom dataTables render it
-        $('.globalDataTable').DataTable();
+        //$('.globalDataTable').DataTable();
 });
