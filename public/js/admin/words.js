032a2a9e51878c031a6491b750ce60aa0a0c0459
Luke Hardiman <luke@hardiman.co.nz>
2015-10-27 18:08:42 +1300

    Updating Admin -Modals for editing and removing.

--- public/js/admin/words.js
+++ public/js/admin/words.js
@@ -2,21 +2,48 @@
  * Created by 9901623 on 21/10/2015.
  */
 $(document).ready(function(){
-    //bb
-    $('#wordLists').DataTable();
+    var editDetails = null; //global
 
+    $('#wordLists').DataTable();
     //words modal
-    var editWord = $("#editWord");
+    var editWordModal = $("#editWord");
 
     //bind our buttons
-    $(editWord).on('show.bs.modal', function(e) {
-        var editDetails = $(e.relatedTarget).data('record');
-        console.log('editDetails',editDetails)
-        $(this).find('.danger').click(function(e){
-            console.log('closing');
+    $(editWordModal).on('show.bs.modal', function(e) {
+        //grab the details and render into the modal
+        editDetails = $WordsList[$(e.relatedTarget).data('record_id')];
+
+        //inputs should match :) which they allowed us to use angularJS
+        $.each(editDetails,function(key, value){
+            $('#' + key,editWordModal).val(value);
         });
+        console.log('editDetails',editDetails)
+
     });
 
+    //detroy modal
+    $('#close',editWordModal).click(function(e){
+        console.log('closing killing editDetails');
+        editDetails = null;
+        return e.preventDefault();
+    });
+
+    //save our entry details and update server
+    $('#save',editWordModal).click(function(e){
+
+        //grab all the input fields from our modal
+        var entry = $('.entry',editWordModal);
+
+        //lets update editDetails object with new entry details
+        $.each(entry,function(key, value){
+            editDetails[$(value).attr('id')] = $(value).val()
+        });
+
+        console.log("editDetails",editDetails);
+        //lets post to server and update
+
+        return e.preventDefault();
+    });
 });
 
 //bind our edit functions to edit modal
