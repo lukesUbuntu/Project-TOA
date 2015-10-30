5432be26d4287a69e4f8cac10ae31155184d515b
lukesUbuntu <luke@hardiman.co.nz>
2015-10-30 22:10:19 +1300

    Updating prod repo -Changed games

--- games/GuessTeReo/functions/quickConnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("127.0.0.1", "guessTeReo", "myadmin123", "guessTeReo");
-if (mysqli_connect_errno()) {
-    //echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
--- games/GuessTeReo/functions/securityCheck.php
+++
@@ -1,9 +0,0 @@
-<!--If user isn't logged into the system and tries to directly play from the URL, the user will be forced back to the
-login screen to register or login.-->
-<?php
-    session_start();
-    if (!isset($_SESSION['getUser.php'])) {
-        header("Location: ../../../index.php");
-        die();
-    }
-?>
